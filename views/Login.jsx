dfb1cabf61875674f127b399777fd5825085f438
IvanPlaza44 <plazaivan44@gmail.com>
2026-05-06 16:10:45 -0300

    Register

--- views/Login.jsx
+++ views/Login.jsx
@@ -1,69 +1,44 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Eye, EyeOff, Lock, Mail } from "lucide-react";
+import { Eye, EyeOff, Lock, ShieldUser } from "lucide-react";
 
 export default function Login() {
 
-  const [email, setEmail] = useState("");
+  const [surname, setSurname] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    setIsLoading(true);
+  const handleSubmit = ()=>{}
 
-    if (!email || !password) {
-      setError("Por favor, completa todos los campos");
-      setIsLoading(false);
-      return;
-    }
-
-    await new Promise((resolve) => setTimeout(resolve, 500));
-
-    const success = login(email, password, role);
-    if (success) {
-      navigate(role === "seller" ? "/dashboard" : "/");
-    } else {
-      setError("Credenciales inválidas");
-    }
-    setIsLoading(false);
-  };
 
   return (
     <div className="container mx-auto flex min-h-screen items-center justify-center px-4 py-16">
       <div className="w-full max-w-md border border-border bg-card rounded-lg shadow-xl overflow-hidden">
         {/* Header */}
         <div className="p-6 text-center space-y-2">
-          <h1 className="text-2xl font-bold text-foreground">Bienvenido de nuevo</h1>
+          <h1 className="text-2xl font-bold text-foreground">Welcome Back</h1>
           <p className="text-muted-foreground text-sm">
-            Inicia sesión en tu cuenta de <span className="text-primary font-bold">POWERFIT</span>
+            Sign in to your <span className="text-primary font-bold">POWERFIT</span> account 
           </p>
         </div>
 
         <form onSubmit={handleSubmit} className="p-6 pt-0 space-y-6">
-          {error && (
-            <div className="rounded-md bg-destructive/10 p-3 text-sm text-destructive border border-destructive/20 animate-in fade-in zoom-in duration-300">
-              {error}
-            </div>
-          )}
 
-          {/* Email */}
+          {/* Surname */}
           <div className="space-y-2">
-            <label htmlFor="email" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-              Email
+            <label htmlFor="Surname" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+              Surname
             </label>
             <div className="relative">
-              <Mail className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+              <ShieldUser className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
               <input
-                id="email"
-                type="email"
+                id="surname"
+                type="surname"
                 required
-                placeholder="tu@ejemplo.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Your Surname"
+                value={surname}
+                onChange={(e) => setSurname(e.target.value)}
                 className="flex h-10 w-full rounded-md border border-input bg-secondary px-10 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 transition-all"
               />
             </div>
@@ -72,7 +47,7 @@ export default function Login() {
           {/* Password */}
           <div className="space-y-2">
             <label htmlFor="password" dir="ltr" className="text-sm font-medium leading-none">
-              Contraseña
+              Password
             </label>
             <div className="relative">
               <Lock className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
@@ -80,7 +55,7 @@ export default function Login() {
                 id="password"
                 type={showPassword ? "text" : "password"}
                 required
-                placeholder="Tu contraseña"
+                placeholder="Your Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="flex h-10 w-full rounded-md border border-input bg-secondary px-10 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary disabled:opacity-50 transition-all"
@@ -102,13 +77,13 @@ export default function Login() {
               disabled={isLoading}
               className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full shadow-lg shadow-primary/20"
             >
-              {isLoading ? "Iniciando sesión..." : "Iniciar Sesión"}
+              {isLoading ? "Loading..." : "Sign in"}
             </button>
             
             <p className="text-center text-sm text-muted-foreground">
-              ¿No tienes cuenta?{" "}
+              Don't have an account? {" "}
               <Link to="/register" className="text-primary hover:underline font-medium transition-all">
-                Regístrate
+                Register
               </Link>
             </p>
           </div>
